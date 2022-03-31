37e216e9679995c913b50d7f453137ff12cd1a91
Monika Ptasińska <monika.ptasinska98@gmail.com>
2022-04-01 00:01:49 +0200

    added jaxb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,13 @@ module com.ptasinska {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires lombok;
+    requires jakarta.xml.bind;
 
     opens com.ptasinska to javafx.fxml;
     exports com.ptasinska;
     exports com.ptasinska.controller;
+    exports com.ptasinska.data;
     opens com.ptasinska.controller to javafx.fxml;
+    opens com.ptasinska.data to jakarta.xml.bind;
 
 }
