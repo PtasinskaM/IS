1476a0cf404ace2725403c13fee8d188ba7076d9
Monika Ptasińska <monika.ptasinska98@gmail.com>
2022-04-01 00:41:26 +0200

    added methods to read/write xml files

--- src/main/java/com/ptasinska/FileUtils.java
+++ src/main/java/com/ptasinska/FileUtils.java
@@ -1,13 +1,20 @@
 package com.ptasinska;
 
 import com.ptasinska.data.Laptop;
+import com.ptasinska.data.Laptops;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
 
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class TxtFileUtils {
+public class FileUtils {
 
     public static final String[] HEADERS = {"Lp.", "nazwa producenta", "przekątna ekranu", "rozdzielczość ekranu",
             "rodzaj powierzchni ekranu", "czy ekran jest dotykowy", "nazwa procesora",
@@ -63,5 +70,41 @@ public class TxtFileUtils {
             System.out.println("Nie znaleziono pliku!");
             return false;
         }
+    }
+
+    public List<Laptop> readFromXml(File file){
+        List<Laptop> result = new ArrayList<>();
+        try {
+            JAXBContext context = JAXBContext.newInstance(Laptops.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Laptops items = (Laptops) unmarshaller.unmarshal(file);
+            for(Laptop item : items.getLaptops()){
+                result.add(item);
+            }
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public boolean saveToXml(List<Laptop> items, File file){
+        try {
+            Laptops laptops = new Laptops();
+            LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Europe/Warsaw"));
+            String dateTimePattern = "yyyy-MM-dd"+" 'T' "+"HH:mm";
+            String moddate = dateTime.format(DateTimeFormatter.ofPattern(dateTimePattern));
+            laptops.setModdate(moddate);
+            laptops.setLaptops(items);
+
+            JAXBContext context = JAXBContext.newInstance(Laptops.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(laptops, file);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
